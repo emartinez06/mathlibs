9fd8e2b28386660ce033b613ec6caa1a54771d11
Edwin M <edwin@devmartinez.net>
2018-02-22 22:32:23 -0400

    mathlibs: Update to Math functions for JS

--- JS/Math.js
+++ JS/Math.js
@@ -5,6 +5,10 @@
 * 2018
 * */
 
+//Variables
+var radius, d, area, circ;
+var pi = 3.14;
+
 function add(a,b){
     return a + b;
 }
@@ -23,4 +27,24 @@ function divide(a,b){
 
 function modulus(a,b){
     return a % b;
+}
+
+function circle_radius(diameter){
+    radius = diameter / 2;
+    return radius;
+}
+
+function circle_diameter(radius){
+    d = radius * 2;
+    return d;
+}
+
+function circle_area(radius){
+    area = pi * radius * radius;
+    return area;
+}
+
+function circumference(diameter){
+    circ = pi / diameter;
+    return circ;
 }
